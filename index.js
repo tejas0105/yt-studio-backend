4e2c7b2b2ab7a0ad12f5eaa6053c55b4ea7f3bae
tejas0105 <tspathak0105@gmail.com>
2024-01-25 17:43:32 +0530

    updated

--- index.js
+++ index.js
@@ -3,24 +3,26 @@ const dotenv = require("dotenv");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const multer = require("multer");
-const uuid = require("uuid");
-
-const storage = multer.diskStorage({
-  destination: "/",
-  filename(req, file, cb) {
-    const newFileName = `${uuid()}-${file.originalname}`;
-    cb(null, newFileName);
-  },
-});
-
-const uploadFile = multer({
-  storage: storage,
-}).single("videofile");
+const fs = require("fs");
+const youtube = require("youtube-api");
+const path = require("path");
 
 const getGoogleOAuthURL = require("./getGoogleURL");
 const getGoogleOAuthTokens = require("./getGoogleOAuthTokens");
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+
+// console.log(storage);
+
 const app = express();
+app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 dotenv.config({ path: `${__dirname}/config.env` });
@@ -50,6 +52,55 @@ app.get("/oauth", async (req, res) => {
 //   });
 //   res.json({message: "cookie sent successfully"})
 // });
+
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("file"), async (req, res) => {
+  try {
+    // console.log(req.body);
+    console.log(req.cookies);
+    try {
+      const postVideo = async () => {
+        const resp = await fetch(
+          `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatus&key=${process.env.API_KEY}`,
+          {
+            method: "POST",
+            headers: {
+              Authorization: `Bearer ${req.cookies.access_token}`,
+              Accept: "application/json",
+              "Content-Type": "application/json application/octet-stream",
+            },
+            body: JSON.stringify({
+              snippet: {
+                description: "Description of uploaded video.",
+                title: "Test video upload.",
+              },
+              status: {
+                privacyStatus: "private",
+              },
+              media: fs.createReadStream(
+                path.join("./uploads", req.file.filename)
+              ),
+            }),
+          }
+        );
+        const data = await resp.json();
+        console.log(data);
+      };
+      postVideo();
+    } catch (error) {
+      console.log(error.message);
+    }
+    // console.log(fs.createReadStream(path.join("./uploads", req.file.filename)));
+
+    return res.status(200).json({
+      status: "success",
+      data: { message: "file successfully received", file: req.file },
+    });
+  } catch (error) {
+    return res.status(400).json({ status: "error", message: "bad request" });
+  }
+});
 
 const port = 3000;
 app.listen(port, () => {
