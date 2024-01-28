032a4afeee037442fa857c8655cafe6d2b214ea1
tejas0105 <tspathak0105@gmail.com>
2024-01-28 08:05:36 +0530

    Moved code to index.js

--- index.js
+++ index.js
@@ -1,111 +1,126 @@
-// const express = require("express");
-// const dotenv = require("dotenv");
-// const cookieParser = require("cookie-parser");
-// const cors = require("cors");
-// const multer = require("multer");
-// const fs = require("fs");
-// const youtube = require("youtube-api");
-// const path = require("path");
-// const { google } = require("googleapis");
+const express = require("express");
+const dotenv = require("dotenv");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const multer = require("multer");
+const fs = require("fs");
+const youtube = require("youtube-api");
+const path = require("path");
+const { google } = require("googleapis");
 
-// const getGoogleOAuthURL = require("./getGoogleURL");
-// const getGoogleOAuthTokens = require("./getGoogleOAuthTokens");
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+dotenv.config({ path: `${__dirname}/config.env` });
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     return cb(null, "./uploads");
-//   },
-//   filename: function (req, file, cb) {
-//     return cb(null, `${Date.now()}-${file.originalname}`);
-//   },
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
+
+// oauth2Client.setCredentials({
+//   access_token:
+//     "ya29.a0AfB_byBP2-xGENzt6ofVTG7-n8iTG7J42Imo1h1ykC9SGtii3WQ0d_VxGOtI5TerwFV84NElYadNVelI5YYb6f8cwVTEQ1v1jIjJj4wHqvCd3S_se9MSGgAY1ELFPvtB837ZllcuLIVsvV8lkDUc1JtP030mXDElaqb_aCgYKAQQSARESFQHGX2MisDfum_2krfOPEFfoTqbL-A0171",
 // });
+// console.log(oauth2Client);
 
-// // console.log(storage);
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    return cb(null, "./uploads");
+  },
+  filename: function (req, file, cb) {
+    return cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
 
-// const app = express();
-// app.use(express.json());
-// app.use(cors());
-// app.use(cookieParser());
-// dotenv.config({ path: `${__dirname}/config.env` });
+const uploadFile = multer({
+  storage: storage,
+}).single("file");
 
-// app.get("/request", async (req, res) => {
-//   let link = await getGoogleOAuthURL();
-//   res.status(200).json({ status: "success", link: link });
-// });
+const scopes = [
+  "https://www.googleapis.com/auth/youtube.readonly",
+  "openid",
+  "https://www.googleapis.com/auth/userinfo.profile",
+  "https://www.googleapis.com/auth/youtube.force-ssl",
+  "https://www.googleapis.com/auth/youtube.upload",
+  "https://www.googleapis.com/auth/youtube",
+  "https://www.googleapis.com/auth/youtubepartner",
+];
 
-// app.get("/oauth", async (req, res) => {
-//   const code = req.query.code;
-//   const { access_token, expires_in, refresh_token } =
-//     await getGoogleOAuthTokens(code);
-//   res.cookie("access_token", access_token, {
-//     expires: new Date(Date.now() + expires_in * 1000),
-//   });
+app.get("/request", async (req, res) => {
+  const url = oauth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: scopes,
+    prompt: "consent",
+  });
+  // console.log(url);
+  // res.send(`<a href=${url}>Sign in</a>`);
+  res.status(200).json({ status: "success", link: url });
+});
 
-//   res.cookie("refresh_token", refresh_token, {
-//     expires: new Date(Date.now() + 3.156e10),
-//   });
-//   //   console.log(data);
-//   res.redirect("http://localhost:5173/dashboard");
-// });
-// // app.get("/data", (req, res) => {
-// //   res.cookie("cookie", "cookie", {
-// //     expires: new Date(Date.now() + 3600000),
-// //   });
-// //   res.json({message: "cookie sent successfully"})
-// // });
+app.get("/oauth", async (req, res) => {
+  const code = req.query.code;
+  const { tokens } = await oauth2Client.getToken(code);
+  const auth = oauth2Client.setCredentials(tokens);
+  console.log(auth);
+  res.cookie("access_token", tokens.access_token, {
+    expires: new Date(tokens.expiry_date),
+  });
+  res.redirect("http://localhost:5173/dashboard");
+});
 
-// const upload = multer({ storage });
+app.post("/upload", uploadFile, async (req, res) => {
+  const service = google.youtube({ version: "v3" });
+  const fileName = req.file.filename;
+  console.log(fileName);
+  console.log(req.body.access_token);
 
-// app.post("/upload", upload.single("file"), async (req, res) => {
-//   try {
-//     // console.log(req.body);
-//     console.log("cookies->", req.cookies);
-//     try {
-//       const postVideo = async () => {
-//         const resp = await fetch(
-//           `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2Cstatus&key=${process.env.API_KEY}`,
-//           {
-//             method: "POST",
-//             headers: {
-//               Authorization: `Bearer ya29.a0AfB_byB8A_pQ27Rhrb43iSo3QM_o9ko4vlNXbAfzlQ-RBkIxTeHR12cQ_rvYR8Eyq9BUvli9f5VHB3MJv-g7IK5KAwsq9S9Xk2j2PaJu2w1wPgOO34KIaVbzDO8lMSrc-b-POyw70zbZZ8zyIqGVQPV3Oo_A7UFommjnaCgYKAWkSARESFQHGX2Mit0iK8UBgcmMS2OWFv_IqjA0171`,
-//               Accept: "multipart/form-data",
-//               "Content-Type": "application/json application/octet-stream",
-//             },
-//             body: JSON.stringify({
-//               snippet: {
-//                 description: "Description of uploaded video.",
-//                 title: "Test video upload.",
-//               },
-//               status: {
-//                 privacyStatus: "private",
-//               },
-//             }),
-//             media: {
-//               body: fs.createReadStream(
-//                 path.join("./uploads", req.file.filename)
-//               ),
-//             },
-//           }
-//         );
-//         const data = await resp.json();
-//         console.log("data->", data);
-//       };
-//       postVideo();
-//     } catch (error) {
-//       console.log(error.message);
-//     }
-//     // console.log(fs.createReadStream(path.join("./uploads", req.file.filename)));
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.CLIENT_ID,
+    process.env.CLIENT_SECRET,
+    process.env.REDIRECT_URI
+  );
 
-//     return res.status(200).json({
-//       status: "success",
-//       data: { message: "file successfully received", file: req.file },
-//     });
-//   } catch (error) {
-//     return res.status(400).json({ status: "error", message: "bad request" });
-//   }
-// });
+  oauth2Client.setCredentials({
+    access_token: `${req.body.access_token}`,
+  });
+  console.log(oauth2Client);
 
-// const port = 3000;
-// app.listen(port, () => {
-//   console.log(`Server started listening on http://localhost:${port}`);
-// });
+  try {
+    const resp = await service.videos.insert({
+      auth: oauth2Client,
+      part: "id,snippet,status",
+      notifySubscribers: false,
+      requestBody: {
+        snippet: {
+          title: `${req.body.title}`,
+          description: `${req.body.description}`,
+        },
+        status: {
+          privacyStatus: "private",
+        },
+      },
+      media: {
+        body: fs.createReadStream(`./uploads/${fileName}`),
+      },
+    });
+    // console.log("resp->", Object.keys(resp));
+    // console.log(auth);
+    return res.status(resp.status).json({
+      status: "success",
+      message: "Video successfully uploaded to YouTube",
+    });
+  } catch (error) {
+    console.log(error.message);
+    return res
+      .status(400)
+      .json({ status: "failed", message: "something went wrong" });
+  }
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server started listening on http://localhost:${port}`);
+});
